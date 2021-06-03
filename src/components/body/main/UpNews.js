3aa0a5910fa25f98238c1d29053a5cdb1f8d3f09
samatzhusup <samat.zhusupov.2018@gmail.com>
2021-06-04 00:18:29 +0600

    read button removed

--- src/components/body/main/UpNews.js
+++ src/components/body/main/UpNews.js
@@ -14,9 +14,7 @@ const UpNews  = (props) =>{
               <div className="col-md-6 col-sm-6 col-xs-12 side-in-image">
                 <div className="event-blog-details">
                   <h4><a href="single-blog.html">{props.title}</a></h4>
-                  <h5>Post By Admin <a><i aria-hidden="true" className="fa fa-heart-o fa-lg" />Likes</a><a><i aria-hidden="true" className="fa fa-comment-o fa-lg" />comments</a></h5>
                   <p>{props.paragraph}</p>
-                  <a className="btn btn-default" href="single-blog.html">Читать</a>
                 </div>
               </div>
             </div>
